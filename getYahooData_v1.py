a9b193011e051eda0157bf2c57b033ca79537c53
natwijaya19 <72418586+natwijaya19@users.noreply.github.com>
2021-12-19 15:25:59 +0700

    Update

---
+++ getYahooData_v1.py
@@ -0,0 +1,18 @@
+#%%
+# Using Pandas-Datareader
+import pandas_datareader as pdr
+
+# Request data via Yahoo public API
+datapdr = pdr.get_data_yahoo ('NVDA')
+
+# Display info
+print(datapdr.info())
+#%%
+# Using yfinance
+import yfinance as yf
+
+# Request historical data for past 5 years
+datayf = yf.Ticker('NVDA').history(period='5y')
+
+# Show info
+print(datayf.info())
